05f97056947c61cedf2148ec602c485f642a05b6
Vinoth-PSD <156505851+Vinoth-PSD@users.noreply.github.com>
2026-04-09 23:22:47 +0530

    vinothmanual change for 404 dms error

--- config/urls.py
+++ config/urls.py
@@ -18,5 +18,5 @@ urlpatterns = [
     path("api/", include("dms.urls")),
 ]
 
-if settings.DEBUG:
+if settings.DEBUG or getattr(settings, "SERVE_MEDIA", False):
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
